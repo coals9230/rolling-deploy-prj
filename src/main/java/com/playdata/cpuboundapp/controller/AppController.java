e8abeb736d0ae3bbb8bceff4ac58997827b384dc
coals9230 <98140128+coals9230@users.noreply.github.com>
2024-07-02 13:23:18 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello~!~!~!";
+        return "<h1 style="color.blue;">hello~!~!~!</h1>";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
